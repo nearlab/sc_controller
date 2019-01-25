c0ded95f2ab33d9cdacfe1be047d2c906595bf39
James Bell <jtb2013@gmail.com>
2019-01-24 20:48:09 -0600

    added test script'

---
+++ test_scripts/testController.sh
@@ -0,0 +1,26 @@
+rostopic pub -1 /orbot/space/vicon geometry_msgs/PoseStamped "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: ''
+pose:
+  position:
+    x: 100.0
+    y: 0.0
+    z: 0.0
+  orientation:
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0"
+
+rostopic pub -1 /orbot/space/state/estimate nearlab_msgs/StateStamped "header:
+  seq: 0
+  stamp: {secs: 0, nsecs: 0}
+  frame_id: ''
+v: {x: 0.0, y: 0.0, z: 0.0}
+w: {x: 0.0, y: 0.0, z: 0.0}
+r: {x: -100.0, y: 0.0, z: 0.0}
+q: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}" 
+
